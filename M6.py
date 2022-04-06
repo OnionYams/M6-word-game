48c7b29a07b8b7d431d424c687b2fb036d58f58d
Y Tang <ytangw03854@gmail.com>
2022-04-06 11:37:00 -0500

    Part 2

--- M6.py
+++ M6.py
@@ -1,4 +1,37 @@
+#M6 python word game
 
-#M6 python
+import random
 
+#global vars controlling the game
+numGuesses = 7
+# 2 versions for a workaround later
+realWord = remainWord = "chippy"
+answer = ""
+for i in range(len(remainWord)):
+    answer += "_"
+letters = []
+
+while (numGuesses > 0):
+    guess = str(input("Guess a letter: "))
+    # validate input
+    if not guess.isalpha():
+        print("Must enter a letter or word")
+        continue
+    if guess in letters:
+        print("Already guessed that")
+        continue
+    guess = guess.lower()
+    if remainWord.find(guess) == -1:
+        numGuesses -= 1
+        print("Incorrect, remaining guesses: " + str(numGuesses))
+        continue
+    # essentially "moves" letter from a duplicate of the true word to the answer so find can be used for repeated letters and 
+    # doesn't always find the first match
+    while remainWord.find(guess) != -1:
+        answer = answer[:remainWord.find(guess)] + guess + answer[remainWord.find(guess)+1:]
+        remainWord = remainWord[:remainWord.find(guess)] + "_" + remainWord[remainWord.find(guess)+1:]
+    if answer == realWord:
+        print("You win. The word was " + realWord)
+        break
+    print(answer)
 
